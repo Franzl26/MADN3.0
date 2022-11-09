f18db812a660698749c02591517d341a3f1a5941
Frank <frank.lucassen@unibw.de>
2022-11-09 14:31:16 +0100

    Enums angepasst

--- madn-client/src/main/java/edu/unibw/sse/madn/clientKomm/AllgemeinerReturnWert.java
+++ madn-client/src/main/java/edu/unibw/sse/madn/clientKomm/AllgemeinerReturnWert.java
@@ -1,5 +1,5 @@
 package edu.unibw.sse.madn.clientKomm;
 
 public enum AllgemeinerReturnWert {
-    RET_VERBINDUNG_ABGEBROCHEN, RET_FEHLER, RET_ERFOLGREICH
+    VERBINDUNG_ABGEBROCHEN, FEHLER, ERFOLGREICH
 }
