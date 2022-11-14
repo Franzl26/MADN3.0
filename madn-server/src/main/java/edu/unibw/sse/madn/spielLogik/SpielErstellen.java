dba5961dabcbb54f1696eccb8af662b09f85f5c2
Markus Konietzka <j0km0023@unibw.de>
2022-11-15 00:02:14 +0100

    edit Beschreibung SpielErstellen

--- madn-server/src/main/java/edu/unibw/sse/madn/spielLogik/SpielErstellen.java
+++ madn-server/src/main/java/edu/unibw/sse/madn/spielLogik/SpielErstellen.java
@@ -1,8 +1,16 @@
 package edu.unibw.sse.madn.spielLogik;
 
-public interface SpielErstellen { // Markus
+/**
+ * Markus
+ */
+public interface SpielErstellen {
     /**
      * Erstellt ein neues Spiel
+     *
+     *Zusammenfassen und Speichern aller relevanten Informationen die benötigt werden um ein neues Spiel zu starten
+     * Zu beginn muss überprüft werden, ob die Spieleranzahl in einem Warteraum, inklusive Bots 2, 3 oder 4 Spieler beträgt.
+     * Trifft das nicht zu, erfolgt keine Aktion, ansonsten wird Spiel.startzeitpunkt gespeichert und die Initialwerte für die
+     * Spiel Spieler und Feld geladen und gesetzt (siehe AF_SpielStarten).
      *
      * @param benutzernamen String-Array mit allen im Spiel vertretenen Spielernamen
      * @param bots          Anzahl Bots
